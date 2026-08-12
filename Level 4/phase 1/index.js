67808ecbfd5d6e8b6ff8bd2e6868ef1ac384ede2
shabab-haider <syedshababjafri@gmail.com>
2026-08-12 16:04:14 +0500

    Langchain: llm setup

--- Level 4/phase 1/index.js
+++ Level 4/phase 1/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
+import { ChatGroq } from "@langchain/groq";
 dotenv.config();
 
 const app = express();
@@ -9,33 +10,46 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// without langchain
+
+// app.post("/ai", async (req, res) => {
+//   const { input } = req.body;
+//   const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY,
+//   });
+
+//     const interaction = await ai.interactions.create({
+//       model: "gemini-3.6-flash",
+//       system_instruction: "You are an ai asistant and your name is Jarvis",
+//       input,
+//     });
+//     res.status(200).json({ ai: interaction.output_text });
+
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3.5-flash",
+//     contents: [
+//       {
+//         role: "system",
+//         text: "You are an ai asistant and your name is Jarvis",
+//       },
+//       {
+//         role: "user",
+//         text: input,
+//       },
+//     ],
+//   });
+//   res.status(200).json({ ai: response.text });
+// });
+
+
+const llm = new ChatGroq({
+  model: "llama-3.1-8b-instant",
+});
+
 app.post("/ai", async (req, res) => {
   const { input } = req.body;
-  const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-  });
-
-  //   const interaction = await ai.interactions.create({
-  //     model: "gemini-3.6-flash",
-  //     system_instruction: "You are an ai asistant and your name is Jarvis",
-  //     input,
-  //   });
-  //   res.status(200).json({ ai: interaction.output_text });
-
-  const response = await ai.models.generateContent({
-    model: "gemini-3.5-flash",
-    contents: [
-      {
-        role: "system",
-        text: "You are an ai asistant and your name is Jarvis",
-      },
-      {
-        role: "user",
-        text: input,
-      },
-    ],
-  });
-  res.status(200).json({ ai: response.text });
+  const response = await llm.invoke(input);
+  res.status(200).json({ ai: response.content });
 });
 
 app.get("/", (req, res) => {
